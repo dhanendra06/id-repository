3f3c87ed12f2da23fa92010c61ead1fe9d7852f2
Dhanendra Sahu <dhanendra.tech@gmail.com>
2025-08-06 15:05:10 +0530

    Added fix to improve the performance
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/ObjectStoreHelper.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/ObjectStoreHelper.java
@@ -25,8 +25,6 @@ import io.mosip.idrepository.core.constant.IdRepoErrorConstants;
 import io.mosip.idrepository.core.exception.IdRepoAppException;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.kernel.core.fsadapter.exception.FSAdapterException;
-import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 @Component
 public class ObjectStoreHelper {
@@ -122,11 +120,11 @@ public class ObjectStoreHelper {
 
 	private byte[] getObject(String uinHash, boolean isBio, String fileRefId, String refId) throws IdRepoAppException {
 		String objectName = uinHash + SLASH + (isBio ? BIOMETRICS : DEMOGRAPHICS) + SLASH + fileRefId;
-		try (ResponseInputStream<GetObjectResponse> s3stream = objectStore.getObject(objectStoreAccountName, objectStoreBucketName, null, null, objectName)) {
-			if (s3stream == null) {
+		try (InputStream objStream = objectStore.getObject(objectStoreAccountName, objectStoreBucketName, null, null, objectName)) {
+			if (objStream == null) {
 				throw new IdRepoAppException(FILE_NOT_FOUND);
 			}
-			byte[] encrypted = s3stream.readAllBytes();
+			byte[] encrypted = IOUtils.toByteArray(objStream);
 			return securityManager.decrypt(encrypted, refId);
 		} catch (FSAdapterException | IOException e) {
 			throw new IdRepoAppException(IdRepoErrorConstants.FILE_STORAGE_ACCESS_ERROR, e);
